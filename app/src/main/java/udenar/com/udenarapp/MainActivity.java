adcd4c2ddcd2f772e126697a8ef7e9a0833551c8
admi <kelvin.cadena@gm>
2015-05-12 14:16:14 -0500

    funcio

--- app/src/main/java/udenar/com/udenarapp/MainActivity.java
+++ app/src/main/java/udenar/com/udenarapp/MainActivity.java
@@ -1,5 +1,6 @@
 package udenar.com.udenarapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent r= new Intent(this, MainService.class );
+        startActivity(r);
     }
 
 
