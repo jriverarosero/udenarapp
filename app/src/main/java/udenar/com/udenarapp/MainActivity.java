cf08c9b87cf7cee99feabec4ef8944837ff27ce6
admi <kelvin.cadena@gm>
2015-05-12 16:24:32 -0500

    funcional menu

--- app/src/main/java/udenar/com/udenarapp/MainActivity.java
+++ app/src/main/java/udenar/com/udenarapp/MainActivity.java
@@ -15,6 +15,29 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),MainService.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
     }
 
     public void lanzar(View v){
